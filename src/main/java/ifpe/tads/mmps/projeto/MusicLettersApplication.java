58be81bd12dab7b792666e90c1f52b6bef2fae88
root <root@lucas-Aspire-5750>
2017-10-14 17:43:23 -0300

    Ajuste no inicializador da aplicação

--- src/main/java/ifpe/tads/mmps/projeto/MusicLettersApplication.java
+++ src/main/java/ifpe/tads/mmps/projeto/MusicLettersApplication.java
@@ -1,37 +1,12 @@
 package ifpe.tads.mmps.projeto;
 
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+@SpringBootApplication
+public class MusicLettersApplication {
 
-import ifpe.tads.mmps.projeto.modelo.Musica;
-
-public interface MusicLettersApplication extends JpaRepository<Musica, Long> {
-
-	/**
-	 * Encontra todos as musicas de um mesmo autor.
-	 * 
-	 * @param musica
-	 * @return lista de Musica
-	 */
-	List<Musica> findByAutor(String autor);
-
-	/**
-	 * Encontra um livro a partir do seu título. Retorna uma lista pois podem
-	 * existir mais de um livro com mesmo título.
-	 * 
-	 * @param titulo
-	 * @return lista de livros
-	 */
-	List<Musica> findByTitulo(String titulo);
-
-	/**
-	 * Encontra um livro a partir de seu isbn, como o isbn é único, apenas um livro
-	 * pode ser encontrado.
-	 * 
-	 * @param isbn
-	 * @return livro
-	 */
-	Musica findByIsbn(String isbn);
-
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLettersApplication.class, args);
+	}
 }
